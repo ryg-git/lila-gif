0403fcb6089803a71a07d8fff42d0dbf6766aa91
ryg-git <ygretharekar@outlook.com>
2024-04-13 14:38:08 +0530

    [WIP] add theme and non theme colors row may fix https://github.com/lichess-org/lila-gif/issues/13

--- theme/ch.py
+++ theme/ch.py
@@ -10,6 +10,15 @@ import importlib
 
 ms = importlib.import_module("make-sprites")
 
+NONTHEME_COLORS = [
+    "#262421",  # dark background
+    "#bababa",  # text color
+    "#bf811d",  # title color
+    "#b72fc6",  # bot color
+    "#706f6e",  # 50% text color on dark background
+]
+
+COLOR_WIDTH = SQUARE_SIZE * 2 // 3
 
 def make_ch_sprite(piece_set_name: str):
     svg = ET.Element(
@@ -18,7 +27,7 @@ def make_ch_sprite(piece_set_name: str):
             "xmlns": "http://www.w3.org/2000/svg",
             "version": "1.1",
             "xmlns:xlink": "http://www.w3.org/1999/xlink",
-            "viewBox": f"0 0 {SQUARE_SIZE * 8} {SQUARE_SIZE * 8}",
+            "viewBox": f"0 0 {SQUARE_SIZE * 8} {SQUARE_SIZE * 9}",
         },
     )
 
@@ -27,7 +36,37 @@ def make_ch_sprite(piece_set_name: str):
     for g in ms.make_piece_set(piece_set_name):
         defs.append(ET.fromstring(g))
 
-    for y in range(5):
+
+    for x, color in enumerate(NONTHEME_COLORS):
+        ET.SubElement(
+            svg,
+            "rect",
+            {
+                "x": str(SQUARE_SIZE * 4 + COLOR_WIDTH * x),
+                "y": "0",
+                "width": str(COLOR_WIDTH),
+                "height": str(SQUARE_SIZE),
+                "stroke": "none",
+                "fill": color,
+            },
+        )
+
+
+    ET.SubElement(
+        svg,
+        "rect",
+        {
+            "x": "0",
+            "y": "0",
+            "width": str(SQUARE_SIZE * 4),
+            "height": str(SQUARE_SIZE),
+            "stroke": "none",
+            "fill": "#888888",
+        },
+    )
+
+
+    for y in range(2, 6):
         for x in range(8):
             ET.SubElement(
                 svg,
@@ -69,7 +108,7 @@ def make_ch_sprite(piece_set_name: str):
                     svg,
                     "image",
                     {
-                        "xlink:href": f"ch-numbers/ch{y}.png",
+                        "xlink:href": f"ch-numbers/ch{y - 1}.png",
                         "transform": f"translate({SQUARE_SIZE * (x + 0.60)}, {SQUARE_SIZE * (y + 0.60)})",
                         "width": "15",
                         "height": "15"
@@ -82,7 +121,33 @@ def make_ch_sprite(piece_set_name: str):
             "rect",
             {
                 "x": str(SQUARE_SIZE * x),
-                "y": str(SQUARE_SIZE * 5),
+                "y": str(SQUARE_SIZE),
+                "width": str(SQUARE_SIZE),
+                "height": str(SQUARE_SIZE),
+                "stroke": "none",
+                "fill": "#888888",
+            },
+        )
+
+        color = "w" if x >= 4 else "b"
+
+        ET.SubElement(
+            svg,
+            "use",
+            {
+                "xlink:href": f"#{color}{ms.PIECE_TYPES[1 + x % 4]}",
+                "transform": f"translate({SQUARE_SIZE * x}, {SQUARE_SIZE})",
+                "opacity": "0.1",
+            },
+        )
+
+    for x in range(8):
+        ET.SubElement(
+            svg,
+            "rect",
+            {
+                "x": str(SQUARE_SIZE * x),
+                "y": str(SQUARE_SIZE * 6),
                 "width": str(SQUARE_SIZE),
                 "height": str(SQUARE_SIZE),
                 "stroke": "none",
@@ -97,12 +162,12 @@ def make_ch_sprite(piece_set_name: str):
             "use",
             {
                 "xlink:href": f"#{color}{ms.PIECE_TYPES[0]}",
-                "transform": f"translate({SQUARE_SIZE * x}, {SQUARE_SIZE * 5})",
+                "transform": f"translate({SQUARE_SIZE * x}, {SQUARE_SIZE * 6})",
                 "opacity": "0.1",
             },
         )
 
-    for y in range(6, 8):
+    for y in range(7, 9):
         for x in range(8):
             ET.SubElement(
                 svg,
@@ -117,7 +182,7 @@ def make_ch_sprite(piece_set_name: str):
                 },
             )
 
-            color = "w" if y == 7 else "b"
+            color = "w" if y == 8 else "b"
 
             ET.SubElement(
                 svg,
