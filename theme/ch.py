9b43334698c794e8b867d84d5dbc0b144253f170
ryg-git <ygretharekar@outlook.com>
2024-10-05 18:30:07 +0530

    [WIP] change function to generate crazyhouse sprite https://github.com/lichess-org/lila-gif/issues/13

--- theme/ch.py
+++ theme/ch.py
@@ -1,89 +1,65 @@
 #!/usr/bin/env python3
 
-from chess.svg import SQUARE_SIZE
-
-import io
-import subprocess
 from PIL import Image, ImageDraw
-import xml.etree.ElementTree as ET
-import importlib
 
-ms = importlib.import_module("make-sprites")
+SQUARE_SIZE = 90
 
-NONTHEME_COLORS = [
-    "#262421",  # dark background
-    "#bababa",  # text color
-    "#bf811d",  # title color
-    "#b72fc6",  # bot color
-    "#706f6e",  # 50% text color on dark background
-]
-
-PIECE_SETS = [
-    "alpha",
-    "anarcandy",
-    "caliente",
-    "california",
-    "cardinal",
-    "cburnett",
-    "celtic",
-    "chess7",
-    "chessnut",
-    "cooke",
-    "companion",
-    "disguised",
-    "dubrovny",
-    "fantasy",
-    "fresca",
-    "gioco",
-    "governor",
-    "horsey",
-    "icpieces",
-    "kiwen-suwi",
-    "kosal",
-    "leipzig",
-    "letter",
-    "libra",
-    "maestro",
-    "merida",
-    "monarchy",
-    "mpchess",
-    "pirouetti",
-    "pixel",
-    "reillycraig",
-    "riohacha",
-    "shapes",
-    "spatial",
-    "staunty",
-    "tatiana",
-]
-
-COLOR_WIDTH = SQUARE_SIZE * 2 // 3
-
-def resvg(path):
-    res = subprocess.run(
-        ["resvg", path, "-c", "-w", "90"],
-        stdout=subprocess.PIPE,
-    )
-
-    return Image.open(io.BytesIO(res.stdout), formats=["PNG"])
-
-def resvg_pieces(piece_set):
-    print(f"Preparing {piece_set} pieces...")
-    return {f"{color}{piece}": resvg(f"piece/{piece_set}/{color}{piece}.svg") for color in "wb" for piece in "PNBRQK"}
-
-def make_ch_sprite(piece_set_name: str):
+def make_ch_sprite(pieces, ch_nums):
     image = Image.new("RGB", (8 * SQUARE_SIZE, 8 * SQUARE_SIZE))
     draw = ImageDraw.Draw(image, "RGBA")
 
-    for y in range(2, 6):
-        for x in range(8):
-            rect = (x * SQUARE_SIZE, 0, (x + 1) * SQUARE_SIZE - 1, SQUARE_SIZE * 8 - 1)
-            draw.rectangle(rect, fill=NONTHEME_COLORS[0])
+    fill = "#262421"
 
+    rect = (0, 0, 8 * SQUARE_SIZE, 8 * SQUARE_SIZE)
+
+    draw.rectangle(rect, fill=fill)
+
+    for y in range(5):
+        for i, piece in enumerate("NBRQ"):
+            wpos = (i * SQUARE_SIZE, y * SQUARE_SIZE)
+            wpiece = pieces[f"w{piece}"]
+            image.paste(wpiece, wpos, wpiece)
+
+            bpos = ((4 + i) * SQUARE_SIZE, y * SQUARE_SIZE)
+            bpiece = pieces[f"b{piece}"]
+            image.paste(bpiece, bpos, bpiece)
+    
+    # for y in range(1, 5):
+    #     for x in range(8):
+    #         pos = (x * SQUARE_SIZE + 5, y * SQUARE_SIZE + 5)
+    #         num = ch_nums[x + 1]
+    #         image.paste(num, pos, num)
+
+    wpos = (i * SQUARE_SIZE, y * SQUARE_SIZE)
+    wpiece = pieces[f"w{piece}"]
+    image.paste(wpiece, wpos, wpiece)
+
+    for x in range(4):
+        pos = (x * SQUARE_SIZE, 5 * SQUARE_SIZE)
+        piece = pieces["wP"]
+        image.paste(piece, pos, piece)
+
+    for x in range(4, 8):
+        pos = (x * SQUARE_SIZE, 5 * SQUARE_SIZE)
+        piece = pieces["bP"]
+        image.paste(piece, pos, piece)
+
+    for x in range(8):
+        wpos = (x * SQUARE_SIZE, 6 * SQUARE_SIZE)
+        wpiece = pieces[f"wP"]
+        image.paste(wpiece, wpos, wpiece)
+
+        bpos = (x * SQUARE_SIZE, 7 * SQUARE_SIZE)
+        bpiece = pieces[f"bP"]
+        image.paste(bpiece, bpos, bpiece)
+
+    draw.rectangle((0, 0, 8 * SQUARE_SIZE, SQUARE_SIZE),
+                   fill="#262421AA")
+
+    draw.rectangle((0, 5 * SQUARE_SIZE, 8 * SQUARE_SIZE, 6 * SQUARE_SIZE),
+                   fill="#262421AA")
 
     image = image.convert("RGBA")
     draw = ImageDraw.Draw(image, "RGBA")
-
-    print(f"sprites/crazyhouse-{piece_set_name}.gif")
 
     return image.quantize(64, dither=0)
